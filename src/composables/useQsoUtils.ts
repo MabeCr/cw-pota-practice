77125a3f9ee8170610633e62c80944a7e38dbed4
Chris Mabe <chrismabe2@gmail.com>
2026-03-25 01:22:15 -0400

    Changing the station to accept a State location object. Adding a function to generate a callsign (which needs more work).

--- src/composables/useQsoUtils.ts
+++ src/composables/useQsoUtils.ts
@@ -5,6 +5,52 @@ export function useQsoUtils() {
 
     const rstRegEx: RegExp = new RegExp(/^[1-5][1-9][1-9Nn]$/);
 
+    const callsignTypes: string[] =['1x1', '1x2', '1x3', '2x1', '2x2', '2x3'];
+    const prefixes: string[]= ["k", "n", "w"];
+    const alphabet: string = "abcdefghijklmnopqrstuvwxyz";
+
+    const generateCall = () => {
+        const callType = callsignTypes[Math.floor(Math.random() * callsignTypes.length)];
+
+        let callString: string = '';
+
+        if (callType === '1x1') {
+            callString = prefixes[Math.floor(Math.random() * prefixes.length)] as string;
+            callString += (Math.floor(Math.random() * 10)).toString();
+            callString += alphabet[Math.floor(Math.random() * alphabet.length)] as string;
+        } else if (callType === '1x2') {
+            callString = prefixes[Math.floor(Math.random() * prefixes.length)] as string;
+            callString += (Math.floor(Math.random() * 10)).toString();
+            callString += alphabet[Math.floor(Math.random() * alphabet.length)] as string;
+            callString += alphabet[Math.floor(Math.random() * alphabet.length)] as string;
+        } else if (callType === '1x3') {
+            callString = prefixes[Math.floor(Math.random() * prefixes.length)] as string;
+            callString += (Math.floor(Math.random() * 10)).toString();
+            callString += alphabet[Math.floor(Math.random() * alphabet.length)] as string;
+            callString += alphabet[Math.floor(Math.random() * alphabet.length)] as string;
+            callString += alphabet[Math.floor(Math.random() * alphabet.length)] as string;
+        } else if (callType === '2x1') {
+            callString = prefixes[Math.floor(Math.random() * prefixes.length)] as string;
+            callString += alphabet[Math.floor(Math.random() * alphabet.length)] as string;
+            callString += (Math.floor(Math.random() * 10)).toString();
+            callString += alphabet[Math.floor(Math.random() * alphabet.length)] as string;
+        } else if (callType === '2x2') {
+            callString = prefixes[Math.floor(Math.random() * prefixes.length)] as string;
+            callString += alphabet[Math.floor(Math.random() * alphabet.length)] as string;
+            callString += (Math.floor(Math.random() * 10)).toString();
+            callString += alphabet[Math.floor(Math.random() * alphabet.length)] as string;
+            callString += alphabet[Math.floor(Math.random() * alphabet.length)] as string;
+        } else if (callType === '2x3') {
+            callString = prefixes[Math.floor(Math.random() * prefixes.length)] as string;
+            callString += alphabet[Math.floor(Math.random() * alphabet.length)] as string;
+            callString += (Math.floor(Math.random() * 10)).toString();
+            callString += alphabet[Math.floor(Math.random() * alphabet.length)] as string;
+            callString += alphabet[Math.floor(Math.random() * alphabet.length)] as string;
+            callString += alphabet[Math.floor(Math.random() * alphabet.length)] as string;
+        }
+        return callString;
+    };
+
     const validateCall = (callString: string) => {
         return callRegEx.test(callString);
     };
@@ -17,5 +63,5 @@ export function useQsoUtils() {
         return US_STATES.some(state => state.code.toLowerCase() === stateCode.toLocaleLowerCase());
     }
 
-    return { validateCall, validateRST, validateState };
+    return { generateCall, validateCall, validateRST, validateState };
 }
--- src/types/station.ts
+++ src/types/station.ts
@@ -2,7 +2,7 @@ import type { QsoSteps} from "@/constants/qsoStates";
 
 export interface Station {
     callsign: string;
-    state: string;
+    state: { code: string, name: string };
     park2parkID: string | null;
     qsoStep: QsoSteps;
 }
